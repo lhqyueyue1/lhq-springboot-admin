9555b32745ed82c24ac4a6123515de23169e2e19
lhq <lhq@gxuwz.com>
2022-02-06 15:06:55 +0800

    Initial commit

---
+++ src/main/java/com/gxuwu/admin/LhqSpringbootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.gxuwu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhqSpringbootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhqSpringbootAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxuwu/admin/LhqSpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxuwu.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhqSpringbootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
